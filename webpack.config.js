fd20ada1fee1feddffa47fc01a492099ddd1f066
kubajj <vilouskubajj@gmail.com>
2019-03-07 14:43:21 +0100

    Change public path to ./

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: './',
     filename: 'build.js'
   },
   module: {
@@ -59,6 +59,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Computatis",
+      template: 'index.html'
       // doplnit další podle
       // https://github.com/jantimon/html-webpack-plugin#options
     })
