95617bf1464b918d5939a057a74fd89b302c4102
kubajj <vilouskubajj@gmail.com>
2019-03-09 16:37:15 +0100

    Delete history mode

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.use(VueRouter);
 export const bus = new Vue();
 const router = new VueRouter({
 	routes: Routes,
-	mode: 'history'
+	/*mode: 'history'*/
 });
 
 Vue.component('app-navigation', Navigation);
