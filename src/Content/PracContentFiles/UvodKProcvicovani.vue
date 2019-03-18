d6dbf599f396e6e7d9ae6575f0a35b8419c09dfe
kubajj <vilouskubajj@gmail.com>
2019-03-18 19:04:10 +0100

    Correction of a typo

--- src/Content/PracContentFiles/UvodKProcvicovani.vue
+++ src/Content/PracContentFiles/UvodKProcvicovani.vue
@@ -7,7 +7,7 @@
 		</b-row>
 		<b-row>
 			<b-col cols="12" sm='12' md='12' lg='10' xl='8'>
-				<p>Můžete si tu zvolit jednotlivé okruhy příkladů nebo si vytvořit vlastní generátor. Pro druhou variantu nahlédněte dodokumentace na&nbsp<a href="https://gitlab.com/kubajj/computatis" target='_blank'>gitlabu</a> nebo na&nbsp<a href="https://github.com/kubajj/computatis" target='_blank'>githubu</a>.</p>
+				<p>Můžete si tu zvolit jednotlivé okruhy příkladů nebo si vytvořit vlastní generátor. Pro druhou variantu nahlédněte do dokumentace na&nbsp<a href="https://gitlab.com/kubajj/computatis" target='_blank'>gitlabu</a> nebo na&nbsp<a href="https://github.com/kubajj/computatis" target='_blank'>githubu</a>.</p>
 			</b-col>
 		</b-row>		
 	</div>
