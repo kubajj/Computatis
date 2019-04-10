306b793668480a04e816e77bce34a4f2cb767d9a
kubajj <vilouskubajj@gmail.com>
2019-04-10 21:42:07 +0200

    Improve styling on the homepage

--- src/Content/HomeContent.vue
+++ src/Content/HomeContent.vue
@@ -5,7 +5,7 @@
 		<b-col cols='12' sm='12' md='10' lg='8' xl='8'>
 	  		<b-jumbotron class='content'>
 	  			<h1 class='title'>Domovská stránka</h1>
-	    		<p style="text-align: left;">Tato webová aplikace umožňuje uživateli procvičovat si matematiku. Obsahuje několik příkladů, které jsou děleny do kategorií podle jevu, na který se zaměřují. Každá otázka má odpověď, která lze jednoduše zapsat, aby se také jednoduše dala zkontrolovat, což aplikace také ovládá. Po potvrzení odeslání výsledku se dozvíte, zda je váš výsledek správný, či chybný.<br>V případě jakýchkoliv dotazů mě neváhejte kontaktovat na emailové adrese vilouskubajj@gmail.com.</p>
+	    		<p style="text-align: center;">Tato webová aplikace umožňuje uživateli procvičovat si matematiku. Obsahuje několik příkladů, které jsou děleny do kategorií podle jevu, na který se zaměřují. Každá otázka má odpověď, která lze jednoduše zapsat, aby se také jednoduše dala zkontrolovat, což aplikace také ovládá.<br>Po potvrzení odeslání výsledku se dozvíte, zda je váš výsledek správný, či chybný.p>
 			</b-jumbotron>
 		</b-col>
 		<b-col cols='2'></b-col>
