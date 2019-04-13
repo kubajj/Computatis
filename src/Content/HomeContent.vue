6b572df722a0c95c43f4d573adb255fb0dfffec4
kubajj <vilouskubajj@gmail.com>
2019-04-13 22:31:37 +0200

    Add button with link to 'procvičování' to homepage

--- src/Content/HomeContent.vue
+++ src/Content/HomeContent.vue
@@ -5,7 +5,8 @@
 		<b-col cols='12' sm='12' md='10' lg='8' xl='8'>
 	  		<b-jumbotron class='content'><!-- Text -->
 	  			<h1 class='title'>Domovská stránka</h1>
-	    		<p style="text-align: center;">Tato webová aplikace umožňuje uživateli procvičovat si matematiku. Obsahuje několik příkladů, které jsou děleny do kategorií podle jevu, na který se zaměřují. Každá otázka má odpověď, která lze jednoduše zapsat, aby se také jednoduše dala zkontrolovat, což aplikace také ovládá.<br>Po potvrzení odeslání výsledku se dozvíte, zda je váš výsledek správný, či chybný</p>
+	    		<p style="text-align: center;">Tato webová aplikace umožňuje uživateli procvičovat si matematiku. Obsahuje několik příkladů, které jsou děleny do kategorií podle jevu, na který se zaměřují. Každá otázka má odpověď, která lze jednoduše zapsat, aby se také jednoduše dala zkontrolovat, což aplikace také ovládá.<br>Po potvrzení odeslání výsledku se dozvíte, zda je váš výsledek správný, či chybný</p><br>
+	    		<p>Zde můžete přistoupit k <b-button><router-link to='/procvicovani' style='color: white'>procvičování</router-link></b-button>.</p> 		
 			</b-jumbotron>
 		</b-col>
 		<b-col cols='2'></b-col>
